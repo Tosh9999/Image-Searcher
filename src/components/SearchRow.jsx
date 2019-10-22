3cbfcaf85cc5f765c6eb54aa38f1d73320a16d2e
David Pimentel <davidjohnpimentel@gmail.com>
2019-10-22 15:46:45 -0500

    Still has many issues, but basic functionality at last works

--- src/components/SearchRow.jsx
+++ src/components/SearchRow.jsx
@@ -7,35 +7,7 @@ class SearchRow extends Component {
     this.state = { isFetching: false, imageUrls: [] };
   }
 
-  componentDidMount() {
-    this.setState({ isFetching: true });
-    const {
-      searchTerm,
-      api_key,
-      search_engine_id,
-      results_per_term,
-      safe_search_level,
-      picture_rights
-    } = this.props;
-    var searchData = this.performSearch(
-      searchTerm,
-      api_key,
-      search_engine_id,
-      results_per_term,
-      safe_search_level,
-      picture_rights
-    );
-
-    var urls = this.parseImageUrls(searchData);
-    this.setState({ imageUrls: urls, isFetching: false });
-  }
-
-  parseImageUrls(searchData) {
-    var urls = searchData.items.map(item => item.link);
-    return urls;
-  }
-
-  performSearch(
+  fetchSearchData(
     searchTerm,
     api_key,
     search_engine_id,
@@ -43,14 +15,48 @@ class SearchRow extends Component {
     safe_search_level,
     picture_rights
   ) {
-    return { "hardcode goes": "here" };
+    this.setState({ isFetching: true });
+    var queryString = `q=${encodeURIComponent(
+      searchTerm
+    )}&key=${encodeURIComponent(api_key)}&cx=${encodeURIComponent(
+      search_engine_id
+    )}&searchType=image&num=${encodeURIComponent(
+      results_per_term
+    )}&safe=${encodeURIComponent(
+      safe_search_level
+    )}&rights=${encodeURIComponent(picture_rights)}`;
+    var completeSearchURL = `https://www.googleapis.com/customsearch/v1?${queryString}`;
+    fetch(completeSearchURL).then(response =>
+      response.json().then(this.assignImageUrls.bind(this))
+    );
+    //
+  }
+
+  assignImageUrls(searchDataJson) {
+    var urls = searchDataJson.items.map(item => item.link);
+    this.setState({ imageUrls: urls, isFetching: false });
+  }
+
+  componentDidMount() {
+    this.fetchSearchData(
+      this.props.searchTerm,
+      this.props.api_key,
+      this.props.search_engine_id,
+      this.props.results_per_term,
+      this.props.safe_search_level,
+      this.props.picture_rights
+    );
   }
 
   render() {
     return (
       <tr>
         <th>{this.props.searchTerm}</th>
-        {this.state.isFetching && <h3>Fetching search data...</h3>}
+        {this.state.isFetching && (
+          <th>
+            <h3>Fetching search data...</h3>
+          </th>
+        )}
         {this.state.imageUrls.map(url => (
           <td key={url}>
             <SearchedImage key={url} imageUrl={url} />
